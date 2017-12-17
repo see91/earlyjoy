1396d0103eff01d35eb1ac011d9b06b234fab210
siaohu1 <87170508@qq.com>
2017-12-17 09:42:00 +0800

    test

--- client/container/index/index.jsx
+++ client/container/index/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import actions from '../../redux/actions/index.js';
+import {ajax} from "../../util/index";
 
 @connect((state) => {
   return {
@@ -11,28 +12,30 @@ export default class extends Component {
   constructor () {
     super();
     this.state = {
-
     };
   }
 
-  clickHandler () {
-    this.props.setCnt();
+  componentDidMount() {
+    ajax({
+      url:'http://localhost:8333/api/myinfo',
+      method:'get'
+    }).then(res=>{
+      let {userName,avatar} = res;
+      this.setState({userName,avatar})
+    }).catch(err=>{
+      }
+    )
   }
-
-
   render () {
     let { cnt } = this.props.cntData;
-
+    let {userName,avatar} = this.state;
     return (
-        <div className="page-wrap main-page" ref="mainPage">
-          首页
-          <br/>
-          <span>使用 react global state(redux store) 切换路由后数据还在</span>
-          <br/>
-          <button onClick={this.clickHandler.bind(this)}>点击数量加一</button>
-          <br/>
-          <span>数量：</span><span>{cnt}</span>
+      <div className="page-wrap main-page" ref="mainPage">
+        <div className="user-info-wrap">
+          <img className="user-avatar" src={avatar}/>
+          <p className="user-name">{userName}</p>
         </div>
+      </div>
     )
   }
 }
--- client/container/rank/rank.jsx
+++ client/container/rank/rank.jsx
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
+import './rank.less';
+import {ajax} from "../../util";
 
 export default class extends Component {
   constructor () {
     super();
     this.state = {
-
+      userName:[]
     };
   }
-
   componentWillMount () {
-
+    ajax({
+      url:'http://localhost:8333/api/ranklist',
+      method:'post',
+      headers:{'Content-Type':"application/json"},
+      // data:{userName},
+    }).then(res=>{
+      let {list,hasMore} =res;
+      console.log(res);
+      this.setState({list,hasMore})
+    }).catch(err=>{
+      console.log('222222')
+      }
+    )
   }
-
   render () {
     return (
-      <div className="page-wrap rank-page">
-        排行榜
+      <div className="rank-page">
+          <ul className="rank-list">
+            <li className="rank-item" >
+              <span className="rank-level">1</span>
+              <img className="rank-avatar"/>
+              <p className="rank-continued">连续起床777天</p>
+            </li>
+
+          </ul>
       </div>
     )
   }
 }
-import './rank.less';
+
